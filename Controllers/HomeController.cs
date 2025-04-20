ebc47ffe65cf90a8ea9d33abf757ea6bd05079ca
Øystein Buvik <ohbuvik@gmail.com>
2025-04-20 19:45:58 +0200

    4A spillet fungerer

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,4 +28,9 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult Spill2()
+{
+    return View();
+}
 }
