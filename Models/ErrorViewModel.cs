e6cddd24a244ac1cc4fa0458b7a45110f518c7b4
Øystein Buvik <ohbuvik@gmail.com>
2025-04-20 20:03:27 +0200

    Spillet virker

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TSD2491_oblig1_ferhat_oystein.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
